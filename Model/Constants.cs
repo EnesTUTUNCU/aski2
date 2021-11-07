1479fa46c4b0d20418ec9d0cc6e93cf158990ff7
EnesTUTUNCU <34237963+EnesTUTUNCU@users.noreply.github.com>
2021-11-08 00:45:51 +0300

    Add files via upload

---
+++ Model/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace askiapp.Model
+{
+    public class Constants
+    {
+        public const string SecretKey = "_12Ga3%acb!!asKT5.As&-'*!.@aiHf.ayGP0+2.1:@_5:faRAsGHL14!Esa%%1rAb_S1tadg31ASHKaS_m_!a@sgE&!gadg!+%66+!rga52rh2eTg214gTQt4qt4T+Qrtet134t4";
+        public class StorageVariables
+        {
+            public const string Token = "G%.1%-*4g.63_.14!";
+            public const string LoggedAccount = "a%1.6-q4!+.fgLTq@rT";
+        }
+    }
+}
---
+++ Model/Requests/MerchantLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace askiapp.Model.Requests
+{
+    public class MerchantLogin
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
